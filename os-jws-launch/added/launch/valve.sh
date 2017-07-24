5ae8935720656b9545977a336adcd9e9f1cec177
Bill Decoste <wdecoste@redhat.com>
2017-07-24 08:36:55 -0700

    CLOUD-1814 CLOUD-1820 RemoteIpValve and X-Forewarded-For

--- os-jws-launch/added/launch/valve.sh
+++ os-jws-launch/added/launch/valve.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+function prepareEnv() {
+  unset DEBUG
+  unset DISABLE_REMOTE_IP_VALVE
+}
+
 function configure() {
   configure_error_valve
+  configure_remote_ip_valve
 }
 
 configure_error_valve() {
@@ -13,3 +19,12 @@ configure_error_valve() {
     sed -i "s|##TOMCAT_SHOW_SERVER_INFO##|false|" $JWS_HOME/conf/server.xml
   fi
 }
+
+configure_remote_ip_valve() {
+  if [ "$DISABLE_REMOTE_IP_VALVE" == "true" ]; then
+    sed -i "s|<!-- ##REMOTE_IP_VALVE## -->||" $JWS_HOME/conf/server.xml
+  else
+    sed -i "s|<!-- ##REMOTE_IP_VALVE## -->|<Valve className=\"org.apache.catalina.valves.RemoteIpValve\" remoteIpHeader=\"X-Forwarded-For\" protocolHeader=\"X-Forwarded-Proto\"/>|" $JWS_HOME/conf/server.xml
+  fi
+}
+
