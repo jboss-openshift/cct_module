c44657a187c104bd0b8253815ee4ea2bb2a039ce
Filippe Spolti <fspolti@redhat.com>
2017-08-10 17:00:07 -0300

    [CLOUD-1859] - Install non-kjar jars in local repo

--- os-kieserver-launch/added/kieserver-install.sh
+++ os-kieserver-launch/added/kieserver-install.sh
@@ -14,29 +14,31 @@ MAVEN_REPO=${HOME}/.m2/repository
 mkdir -p ${MAVEN_REPO}
 
 if [ -d ${DEPLOY_DIR} ]; then
-    TEMP_KJARS_DIR="${LOCAL_SOURCE_DIR}/tmp-kjars"
+    TEMP_JARS_DIR="${LOCAL_SOURCE_DIR}/tmp-jars"
 
-    # iterate through all jar files found, and if a kjar (has both kmodule.xml and pom.xml), install into local maven repository
+    # install all jars in the local maven repository, including kjar (has both kmodule.xml and pom.xml)
     for JAR in $(find ${DEPLOY_DIR}/ -maxdepth 1 -name *.jar | sed 's|.*/||'); do
         KJAR=""
-        mkdir -p ${TEMP_KJARS_DIR}/${JAR}
+        mkdir -p ${TEMP_JARS_DIR}/${JAR}
         if [ -d ${DEPLOY_DIR}/${JAR} ]; then
             # jar is an exploded directory; copy contents
-            cp -r ${DEPLOY_DIR}/${JAR}/* ${TEMP_KJARS_DIR}/${JAR}/
+            cp -r ${DEPLOY_DIR}/${JAR}/* ${TEMP_JARS_DIR}/${JAR}/
         else
             # jar is a zipped file; unzip contents
-            unzip -q ${DEPLOY_DIR}/${JAR} -d ${TEMP_KJARS_DIR}/${JAR}
+            unzip -q ${DEPLOY_DIR}/${JAR} -d ${TEMP_JARS_DIR}/${JAR}
         fi
-        if [ -e "${TEMP_KJARS_DIR}/${JAR}/META-INF/kmodule.xml" ]; then
-            POM=$(find ${TEMP_KJARS_DIR}/${JAR}/META-INF/maven -name 'pom.xml' 2>/dev/null)
-            if [ -e "${POM}" ]; then
+
+        # at this moment install all jars on local maven repository
+        POM=$(find ${TEMP_JARS_DIR}/${JAR}/META-INF/maven -name 'pom.xml' 2>/dev/null)
+        if [ -e "${POM}" ]; then
+
+            # verify if the current jar is a kjar
+            if [ -e "${TEMP_JARS_DIR}/${JAR}/META-INF/kmodule.xml" ]; then
+                echo echo "${DEPLOY_DIR}/${JAR} is a kjar."
                 KJAR=${JAR}
             fi
-        fi
-        if [ "${KJAR}" = "" ]; then
-            echo "${DEPLOY_DIR}/${JAR} is not a kjar (skipping)"
-        else
-            echo "${DEPLOY_DIR}/${JAR} is a kjar"
+
+            echo "${DEPLOY_DIR}/${JAR} has a pom: Attempting to install"
             if [ -d ${DEPLOY_DIR}/${JAR} ]; then
                 # jar is an exploded directory; replace with zipped file (for mvn install:install-file below to work)
                 zip -r ${DEPLOY_DIR}/${JAR}.zip ${DEPLOY_DIR}/${JAR}/*
@@ -47,44 +49,45 @@ if [ -d ${DEPLOY_DIR} ]; then
             export MAVEN_OPTS="${MAVEN_OPTS:-$(/opt/run-java/java-default-options)}"
             # Use maven batch mode (CLOUD-579)
             MAVEN_ARGS_INSTALL="-e -DskipTests install:install-file -Dfile=${DEPLOY_DIR}/${JAR} -DpomFile=${POM} -Dpackaging=jar --batch-mode -Djava.net.preferIPv4Stack=true -Popenshift -Dcom.redhat.xpaas.repo.redhatga ${MAVEN_ARGS_APPEND}"
-            echo "Attempting to install kjar with 'mvn ${MAVEN_ARGS_INSTALL}'"
+            echo "Attempting to install jar with 'mvn ${MAVEN_ARGS_INSTALL}'"
             echo "Using MAVEN_OPTS '${MAVEN_OPTS}'"
             echo "Using $(mvn --version)"
-            # Execute the maven install of kjar
+            # Execute the maven install of jar and kjar
             mvn $MAVEN_ARGS_INSTALL
             ERR=$?
             if [ $ERR -ne 0 ]; then
                 echo "Aborting due to error code $ERR from Maven build"
                 # cleanup
-                rm -rf ${TEMP_KJARS_DIR}
+                rm -rf ${TEMP_JARS_DIR}
                 exit $ERR
             fi
 
-            # Discover KIE_CONTAINER_DEPLOYMENT for when env var not specified
-            pushd $(dirname ${POM}) &> /dev/null
-                # first trigger download of help:evaluate dependencies
-                echo "Inspecting kjar ${DEPLOY_DIR}/${JAR} for artifact information..."
-                # Add JVM default options
-                export MAVEN_OPTS="${MAVEN_OPTS:-$(/opt/run-java/java-default-options)}"
-                # Use maven batch mode (CLOUD-579)
-                MAVEN_ARGS_EVALUATE="--batch-mode -Djava.net.preferIPv4Stack=true -Popenshift -Dcom.redhat.xpaas.repo.redhatga ${MAVEN_ARGS_APPEND}"
-                mvn help:evaluate -Dexpression=project.artifact ${MAVEN_ARGS_EVALUATE}
-                ERR=$?
-                if [ $ERR -ne 0 ]; then
-                    echo "Aborting due to error code $ERR from Maven artifact discovery"
-                    exit $ERR
-                fi
-                # next use help:evaluate to record the kjar as a kie container deployment
-                kieContainerDeploymentsFile="${JBOSS_HOME}/kiecontainer-deployments.txt"
-                kjarGroupId="$(mvn help:evaluate -Dexpression=project.artifact.groupId ${MAVEN_ARGS_EVALUATE} | egrep -v '(^\[.*\])|(Download.*: )')"
-                kjarArtifactId="$(mvn help:evaluate -Dexpression=project.artifact.artifactId ${MAVEN_ARGS_EVALUATE} | egrep -v '(^\[.*\])|(Download.*: )')"
-                kjarVersion="$(mvn help:evaluate -Dexpression=project.artifact.version ${MAVEN_ARGS_EVALUATE} | egrep -v '(^\[.*\])|(Download.*: )')"
-                kieContainerDeployment="${kjarArtifactId}=${kjarGroupId}:${kjarArtifactId}:${kjarVersion}"
-                echo "Adding ${kieContainerDeployment} to ${kieContainerDeploymentsFile}"
-                echo "${kieContainerDeployment}" >> ${kieContainerDeploymentsFile}
-                chmod a+rw ${kieContainerDeploymentsFile}
-            popd &> /dev/null
-
+            # Discover KIE_CONTAINER_DEPLOYMENT for when env var not specified, only kjar (has kmodule.xml) should be configured.
+            if [ "${KJAR}" != "" ]; then
+                pushd $(dirname ${POM}) &> /dev/null
+                    # first trigger download of help:evaluate dependencies
+                    echo "Inspecting kjar ${DEPLOY_DIR}/${JAR} for artifact information..."
+                    # Add JVM default options
+                    export MAVEN_OPTS="${MAVEN_OPTS:-$(/opt/run-java/java-default-options)}"
+                    # Use maven batch mode (CLOUD-579)
+                    MAVEN_ARGS_EVALUATE="--batch-mode -Djava.net.preferIPv4Stack=true -Popenshift -Dcom.redhat.xpaas.repo.redhatga ${MAVEN_ARGS_APPEND}"
+                    mvn help:evaluate -Dexpression=project.artifact ${MAVEN_ARGS_EVALUATE}
+                    ERR=$?
+                    if [ $ERR -ne 0 ]; then
+                        echo "Aborting due to error code $ERR from Maven artifact discovery"
+                        exit $ERR
+                    fi
+                    # next use help:evaluate to record the kjar as a kie container deployment
+                    kieContainerDeploymentsFile="${JBOSS_HOME}/kiecontainer-deployments.txt"
+                    kjarGroupId="$(mvn help:evaluate -Dexpression=project.artifact.groupId ${MAVEN_ARGS_EVALUATE} | egrep -v '(^\[.*\])|(Download.*: )')"
+                    kjarArtifactId="$(mvn help:evaluate -Dexpression=project.artifact.artifactId ${MAVEN_ARGS_EVALUATE} | egrep -v '(^\[.*\])|(Download.*: )')"
+                    kjarVersion="$(mvn help:evaluate -Dexpression=project.artifact.version ${MAVEN_ARGS_EVALUATE} | egrep -v '(^\[.*\])|(Download.*: )')"
+                    kieContainerDeployment="${kjarArtifactId}=${kjarGroupId}:${kjarArtifactId}:${kjarVersion}"
+                    echo "Adding ${kieContainerDeployment} to ${kieContainerDeploymentsFile}"
+                    echo "${kieContainerDeployment}" >> ${kieContainerDeploymentsFile}
+                    chmod a+rw ${kieContainerDeploymentsFile}
+                popd &> /dev/null
+            fi
             # Remove kjar from EAP deployments dir, as KIE loads them from ${HOME}/.m2/repository/ instead.
             # Leaving this file here could cause classloading collisions if multiple KIE Containers are
             # configured for different versions of the same application.
@@ -92,7 +95,7 @@ if [ -d ${DEPLOY_DIR} ]; then
         fi
     done
     # cleanup
-    rm -rf ${TEMP_KJARS_DIR}
+    rm -rf ${TEMP_JARS_DIR}
 
     # Necessary to permit running with a randomised UID
     chown -R jboss:root ${MAVEN_REPO}
