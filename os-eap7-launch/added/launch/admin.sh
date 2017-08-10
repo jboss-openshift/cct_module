20cc95ae8ef065989450cf05265e3fbeba8a0690
Bill Decoste <wdecoste@redhat.com>
2017-08-10 13:05:28 -0700

    cloud-1849 set mgmt security realm if ADMIN_USERNAME/ADMIN_PASSWORD set

--- os-eap7-launch/added/launch/admin.sh
+++ os-eap7-launch/added/launch/admin.sh
@@ -15,7 +15,17 @@ function configureEnv() {
 }
 
 function configure_administration() {
-  if [ -n "${ADMIN_PASSWORD}" ]; then
-        $JBOSS_HOME/bin/add-user.sh -u "$ADMIN_USERNAME" -p "$ADMIN_PASSWORD"
+  if [ -n "${ADMIN_USERNAME}" -a -n "$ADMIN_PASSWORD" ]; then
+    $JBOSS_HOME/bin/add-user.sh -u "$ADMIN_USERNAME" -p "$ADMIN_PASSWORD"
+    if [ "$?" -ne "0" ]; then
+        echo "Failed to create the management realm user $ADMIN_USERNAME"
+        echo "Exiting..."
+        exit
+    fi
+
+    local mgmt_iface_replace_str="security-realm=\"ManagementRealm\""
+    sed -i "s|><!-- ##MGMT_IFACE_REALM## -->| ${mgmt_iface_replace_str}>|" "$CONFIG_FILE"
+  else
+    sed -i "s|##MGMT_IFACE_REALM##||" "$CONFIG_FILE"
   fi
 }
