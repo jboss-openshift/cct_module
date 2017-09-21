da1f7f54834b787dde8026a0d562e30503f7fbf8
Bill Decoste <wdecoste@redhat.com>
2017-09-21 10:09:17 -0700

    CLOUD-1517 configurable JSON logging

--- os-eap7-launch/added/launch/json_logging.sh
+++ os-eap7-launch/added/launch/json_logging.sh
@@ -4,4 +4,10 @@ function configure() {
 
 function configure_json_logging() {
   sed -i "s|^.*\.module=org\.jboss\.logmanager\.ext$||" $LOGGING_FILE
+
+  if [ "${ENABLE_JSON_LOGGING^^}" == "TRUE" ]; then
+    sed -i 's|##CONSOLE-FORMATTER##|OPENSHIFT|' $CONFIG_FILE
+  else
+    sed -i 's|##CONSOLE-FORMATTER##|COLOR-PATTERN|' $CONFIG_FILE
+  fi
 }
