f11b77a98d248a9bcb00483cec88aedf31e5937e
Bill Decoste <wdecoste@redhat.com>
2017-09-26 09:44:51 -0700

    CLOUD-2049 deterministic execution ordering of valves

--- os-eap64-openshift/configure.sh
+++ os-eap64-openshift/configure.sh
@@ -12,3 +12,7 @@ cat ${ADDED_DIR}/standalone.conf >> $JBOSS_HOME/bin/standalone.conf
 
 # Add custom configuration file
 cp -p ${ADDED_DIR}/standalone-openshift.xml $JBOSS_HOME/standalone/configuration/
+
+mkdir -p ${JBOSS_HOME}/standalone/data/content/38/b8ef5d9c683c14b786ba47845934625a1c15d8
+cp ${ADDED_DIR}/jboss-web.xml ${JBOSS_HOME}/standalone/data/content/38/b8ef5d9c683c14b786ba47845934625a1c15d8/content
+
