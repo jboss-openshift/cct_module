3efa267a4a67179aed403bdd4dc4a48ac2069316
Bill Decoste <wdecoste@redhat.com>
2017-10-12 11:03:11 -0700

    CLOUD-2111 support using extensions datasources as tx ObjectStore

--- os-eap7-launch/added/launch/datasource.sh
+++ os-eap7-launch/added/launch/datasource.sh
@@ -11,6 +11,12 @@ function configure() {
 
 function configureEnv() {
   inject_external_datasources
+
+  if [ -n "$JDBC_STORE_JNDI_NAME" ]; then
+    local jdbcStore="<jdbc-store datasource-jndi-name=\"${JDBC_STORE_JNDI_NAME}\"/>"
+    sed -i "s|<!-- ##JDBC_STORE## -->|${jdbcStore}|" $CONFIG_FILE
+  fi
+
 }
 
 function inject_datasources() {
