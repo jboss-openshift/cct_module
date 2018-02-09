befee0d4234e0e4aefb50b99faee95985dde1e8f
Alex Szczuczko <aszczucz@redhat.com>
2018-02-09 14:29:01 -0700

    Sync os-sso72 with os-sso71's changes

--- os-sso72/added/openshift-launch.sh
+++ os-sso72/added/openshift-launch.sh
@@ -1,67 +1,70 @@
 #!/bin/sh
 # Openshift EAP launch script
 
-if [ "${SCRIPT_DEBUG}" = "true" ] ; then
-    set -x
-    echo "Script debugging is enabled, allowing bash commands and their arguments to be printed as they are executed"
-fi
-
-CONFIG_FILE=$JBOSS_HOME/standalone/configuration/standalone-openshift.xml
-LOGGING_FILE=$JBOSS_HOME/standalone/configuration/logging.properties
-
-#For backward compatibility
-ADMIN_USERNAME=${ADMIN_USERNAME:-${EAP_ADMIN_USERNAME:-$DEFAULT_ADMIN_USERNAME}}
-ADMIN_PASSWORD=${ADMIN_PASSWORD:-$EAP_ADMIN_PASSWORD}
-NODE_NAME=${NODE_NAME:-$EAP_NODE_NAME}
-HTTPS_NAME=${HTTPS_NAME:-$EAP_HTTPS_NAME}
-HTTPS_PASSWORD=${HTTPS_PASSWORD:-$EAP_HTTPS_PASSWORD}
-HTTPS_KEYSTORE_DIR=${HTTPS_KEYSTORE_DIR:-$EAP_HTTPS_KEYSTORE_DIR}
-HTTPS_KEYSTORE=${HTTPS_KEYSTORE:-$EAP_HTTPS_KEYSTORE}
-SECDOMAIN_USERS_PROPERTIES=${SECDOMAIN_USERS_PROPERTIES:-${EAP_SECDOMAIN_USERS_PROPERTIES:-users.properties}}
-SECDOMAIN_ROLES_PROPERTIES=${SECDOMAIN_ROLES_PROPERTIES:-${EAP_SECDOMAIN_ROLES_PROPERTIES:-roles.properties}}
-SECDOMAIN_NAME=${SECDOMAIN_NAME:-$EAP_SECDOMAIN_NAME}
-SECDOMAIN_PASSWORD_STACKING=${SECDOMAIN_PASSWORD_STACKING:-$EAP_SECDOMAIN_PASSWORD_STACKING}
-
-IMPORT_REALM_FILE=$JBOSS_HOME/standalone/configuration/import-realm.json
-
-CONFIGURE_SCRIPTS=(
-  $JBOSS_HOME/bin/launch/configure_extensions.sh
-  $JBOSS_HOME/bin/launch/passwd.sh
-  $JBOSS_HOME/bin/launch/datasource.sh
-  $JBOSS_HOME/bin/launch/resource-adapter.sh
-  $JBOSS_HOME/bin/launch/admin.sh
-  $JBOSS_HOME/bin/launch/ha.sh
-  $JBOSS_HOME/bin/launch/jgroups.sh
-  $JBOSS_HOME/bin/launch/https.sh
-  $JBOSS_HOME/bin/launch/json_logging.sh
-  $JBOSS_HOME/bin/launch/security-domains.sh
-  $JBOSS_HOME/bin/launch/jboss_modules_system_pkgs.sh
-  $JBOSS_HOME/bin/launch/deploymentScanner.sh
-  $JBOSS_HOME/bin/launch/ports.sh
-  $JBOSS_HOME/bin/launch/access_log_valve.sh
-  $JBOSS_HOME/bin/launch/add-sso-admin-user.sh
-  $JBOSS_HOME/bin/launch/add-sso-realm.sh
-  $JBOSS_HOME/bin/launch/keycloak-spi.sh
-  $JBOSS_HOME/bin/launch/access_log_valve.sh
-  /opt/run-java/proxy-options
-)
-
-source $JBOSS_HOME/bin/launch/configure.sh
-
-echo "Running $JBOSS_IMAGE_NAME image, version $JBOSS_IMAGE_VERSION"
+source ${JBOSS_HOME}/bin/launch/openshift-common.sh
+source $JBOSS_HOME/bin/launch/logging.sh
 
 # TERM signal handler
 function clean_shutdown() {
-  echo "*** JBossAS wrapper process ($$) received TERM signal ***"
+  log_error "*** JBossAS wrapper process ($$) received TERM signal ***"
   $JBOSS_HOME/bin/jboss-cli.sh -c ":shutdown(timeout=60)"
   wait $!
 }
 
-trap "clean_shutdown" TERM
+function runServer() {
+  local instanceDir=$1
+  local count=$2
+  export NODE_NAME="${NODE_NAME:-node}-${count}"
 
-if [ -n "$SSO_IMPORT_FILE" ] && [ -f $SSO_IMPORT_FILE ]; then
-  $JBOSS_HOME/bin/standalone.sh -c standalone-openshift.xml -bmanagement 127.0.0.1 $JBOSS_HA_ARGS ${JBOSS_MESSAGING_ARGS} -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=${SSO_IMPORT_FILE} -Dkeycloak.migration.strategy=IGNORE_EXISTING ${JAVA_PROXY_OPTIONS} &
+  source $JBOSS_HOME/bin/launch/configure.sh
+
+  log_info "Running $JBOSS_IMAGE_NAME image, version $JBOSS_IMAGE_VERSION"
+
+  trap "clean_shutdown" TERM
+
+  if [ -n "$SSO_IMPORT_FILE" ] && [ -f $SSO_IMPORT_FILE ]; then
+    $JBOSS_HOME/bin/standalone.sh -c standalone-openshift.xml -bmanagement 127.0.0.1 $JBOSS_HA_ARGS -Djboss.server.data.dir="$instanceDir" ${JBOSS_MESSAGING_ARGS} -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=${SSO_IMPORT_FILE} -Dkeycloak.migration.strategy=IGNORE_EXISTING ${JAVA_PROXY_OPTIONS} &
+  else
+    $JBOSS_HOME/bin/standalone.sh -c standalone-openshift.xml -bmanagement 127.0.0.1 $JBOSS_HA_ARGS -Djboss.server.data.dir="$instanceDir" ${JBOSS_MESSAGING_ARGS} ${JAVA_PROXY_OPTIONS} &
+  fi
+
+  PID=$!
+  wait $PID 2>/dev/null
+  wait $PID 2>/dev/null
+}
+
+function init_data_dir() {
+  local DATA_DIR="$1"
+  if [ -d "${JBOSS_HOME}/standalone/data" ]; then
+    cp -rf ${JBOSS_HOME}/standalone/data/* $DATA_DIR
+  fi
+}
+
+if [ "${SPLIT_DATA^^}" = "TRUE" ]; then
+  source /opt/partition/partitionPV.sh
+
+  DATA_DIR="${JBOSS_HOME}/standalone/partitioned_data"
+
+  partitionPV "${DATA_DIR}" "${SPLIT_LOCK_TIMEOUT:-30}"
 else
-  $JBOSS_HOME/bin/standalone.sh -c standalone-openshift.xml -bmanagement 127.0.0.1 $JBOSS_HA_ARGS ${JBOSS_MESSAGING_ARGS} ${JAVA_PROXY_OPTIONS} &
+  source $JBOSS_HOME/bin/launch/configure.sh
+
+  log_info "Running $JBOSS_IMAGE_NAME image, version $JBOSS_IMAGE_VERSION"
+
+  trap "clean_shutdown" TERM
+
+  if [ -n "$CLI_GRACEFUL_SHUTDOWN" ] ; then
+    trap "" TERM
+    log_info "Using CLI Graceful Shutdown instead of TERM signal"
+  fi
+
+  if [ -n "$SSO_IMPORT_FILE" ] && [ -f $SSO_IMPORT_FILE ]; then
+    $JBOSS_HOME/bin/standalone.sh -c standalone-openshift.xml -bmanagement 127.0.0.1 $JBOSS_HA_ARGS ${JBOSS_MESSAGING_ARGS} -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=${SSO_IMPORT_FILE} -Dkeycloak.migration.strategy=IGNORE_EXISTING ${JAVA_PROXY_OPTIONS} &
+  else
+    $JBOSS_HOME/bin/standalone.sh -c standalone-openshift.xml -bmanagement 127.0.0.1 $JBOSS_HA_ARGS ${JBOSS_MESSAGING_ARGS} ${JAVA_PROXY_OPTIONS} &
+  fi
+
+  PID=$!
+  wait $PID 2>/dev/null
+  wait $PID 2>/dev/null
 fi
-wait $!
