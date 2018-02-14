08543c4b79112f0663af2e083683655b0f6552ea
Osni Oliveira <osni.oliveira@gmail.com>
2018-02-14 17:05:16 -0200

    [CLOUD-2226] Use SYM_ENCRYPT instead of ENCRYPT
    The ENCRYPT protocol was deprecated and should not be used anymore.
    JDG is using SYM_ENCRYPT since JDG 7.1.0.
    
    Signed-off-by: Osni Oliveira <osni.oliveira@gmail.com>

---
+++ os-jdg7-launch/added/launch/jgroups.sh
@@ -0,0 +1,39 @@
+# only processes a single environment as the placeholder is not preserved
+
+function prepareEnv() {
+  unset JGROUPS_ENCRYPT_SECRET
+  unset JGROUPS_ENCRYPT_PASSWORD
+  unset JGROUPS_ENCRYPT_KEYSTORE_DIR
+  unset JGROUPS_ENCRYPT_KEYSTORE
+  unset JGROUPS_ENCRYPT_NAME
+}
+
+function configure() {
+  configure_jgroups_encryption
+}
+
+function configureEnv() {
+  configure
+}
+
+function configure_jgroups_encryption() {
+  jgroups_encrypt=""
+
+  if [ -n "${JGROUPS_ENCRYPT_SECRET}" ]; then
+    if [ -n "${JGROUPS_ENCRYPT_NAME}" -a -n "${JGROUPS_ENCRYPT_PASSWORD}" ] ; then
+      jgroups_encrypt="\
+        <protocol type=\"SYM_ENCRYPT\">\
+          <property name=\"provider\">SunJCE</property>\
+          <property name=\"sym_algorithm\">AES</property>\
+          <property name=\"encrypt_entire_message\">true</property>\
+          <property name=\"keystore_name\">${JGROUPS_ENCRYPT_KEYSTORE_DIR}/${JGROUPS_ENCRYPT_KEYSTORE}</property>\
+          <property name=\"store_password\">${JGROUPS_ENCRYPT_PASSWORD}</property>\
+          <property name=\"alias\">${JGROUPS_ENCRYPT_NAME}</property>\
+        </protocol>"
+    else
+      echo "WARNING! Partial JGroups encryption configuration, the communication within the cluster WILL NOT be encrypted."
+    fi
+  fi
+
+  sed -i "s|<!-- ##JGROUPS_ENCRYPT## -->|$jgroups_encrypt|g" "$CONFIG_FILE"
+}
