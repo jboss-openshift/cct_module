213a8a36bb476118ebf9280efe938797af0dbd8e
rcernich <rcernich@redhat.com>
2018-02-14 09:59:43 -0700

    CLOUD-2305 reset attributes instead of reverting to black for log messages
    Signed-off-by: rcernich <rcernich@redhat.com>

--- os-logging/added/launch/logging.sh
+++ os-logging/added/launch/logging.sh
@@ -1,17 +1,18 @@
 BLACK='\033[0;30m'
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
+DEFAULT='\033[0m'
 
 function log_warning() {
   local message="${1}"
 
-  echo >&2 -e "${YELLOW}WARN ${message}${BLACK}"
+  echo >&2 -e "${YELLOW}WARN ${message}${DEFAULT}"
 }
 
 function log_error() {
   local message="${1}"
 
-  echo >&2 -e "${RED}ERROR ${message}${BLACK}"
+  echo >&2 -e "${RED}ERROR ${message}${DEFAULT}"
 }
 
 function log_info() {
