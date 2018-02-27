a9ea99863e8659c905a9aabb066ef062ae4f1846
Jonathan Dowland <jdowland@redhat.com>
2018-02-27 10:13:21 +0000

    CLOUD-2235 remove kieserver modules and tests
    https://issues.jboss.org/browse/CLOUD-2235
    
    They are moving to this repo https://github.com/jboss-container-images/jboss-kie-modules
    
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- os-kieserver-launch/added/ha.sh
+++
@@ -1,6 +0,0 @@
-
-function preConfigure() {
-  # kieserver does not support clustering, so hard-code to legacy configuration.
-  sed -i "s|<!-- ##JGROUPS_PING_PROTOCOL## -->|<protocol type=\"openshift.KUBE_PING\"/>|g" $CONFIG_FILE
-}
-
