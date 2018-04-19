cf05b91b286188cb7094a8621e67fcd31a19c99e
Ruben Romero Montes <rromerom@redhat.com>
2018-04-19 22:36:55 +0200

    [CLOUD-2484] Refactor maven-repos.sh and include unit tests
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

---
+++ tests/bats/common/xml_utils.bash
@@ -0,0 +1,17 @@
+function assert_xml() {
+  local file=$1
+  local xpath=$2
+  local expected=$3
+  local xml=$(xmllint --xpath $xpath $file)
+
+  diff <(echo $xml | xmllint --format -) $expected
+}
+
+function assert_xml_value() {
+  local file=$1
+  local xpath=$2
+  local expected=$3
+  local actual=$(xmllint --xpath $xpath $file)
+  echo $xpath > /tmp/xml
+  [[ $actual =~ $expected ]]
+}
