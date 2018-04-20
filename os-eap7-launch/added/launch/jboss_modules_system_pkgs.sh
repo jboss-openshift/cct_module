0effb35298e7d413f7fd911c778d98d886503501
Ken Wills <ken@zaptillion.net>
2018-04-20 15:29:38 -0500

    add com.sun.crypto.provider to JBOSS_MODULES_SYSTEM_PKGS

--- os-eap7-launch/added/launch/jboss_modules_system_pkgs.sh
+++ os-eap7-launch/added/launch/jboss_modules_system_pkgs.sh
@@ -8,7 +8,7 @@ function configure() {
 }
 
 function configure_jboss_modules_system_pkgs() {
-  JBOSS_MODULES_SYSTEM_PKGS="org.jboss.logmanager,jdk.nashorn.api"
+  JBOSS_MODULES_SYSTEM_PKGS="org.jboss.logmanager,jdk.nashorn.api,com.sun.crypto.provider"
 
   if [ -n "$JBOSS_MODULES_SYSTEM_PKGS_APPEND" ]; then
     JBOSS_MODULES_SYSTEM_PKGS="$JBOSS_MODULES_SYSTEM_PKGS,$JBOSS_MODULES_SYSTEM_PKGS_APPEND"
