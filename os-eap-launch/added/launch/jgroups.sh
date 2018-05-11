5513dca426ebb0f215d507642f5f8fa4715c4878
Jan Lieskovsky <jlieskov@redhat.com>
2018-05-11 19:31:29 +0200

    [CLOUD-2437] Introduce the ability to set JGroups protocol type to ASYM_ENCRYPT
    Perform this by introduction of the new JGROUPS_ENCRYPT_PROTOCOL environment
    variable, which can accept one of the two possible values:
    
    * Either SYM_ENCRYPT (the default setting)
    * Or ASYM_ENCRYPT
    
    Raise a warning, if 'ASYM_ENCRYPT' protocol was chosen, but JCEKS
    JGroups keystore definition exists
    
    Also describe the new JGROUPS_ENCRYPT_PROTOCOL environment variable
    in the module.yaml definition of the os-eap-launch module
    
    Signed-off-by: Jan Lieskovsky <jlieskov@redhat.com>

--- os-eap-launch/added/launch/jgroups.sh
+++ os-eap-launch/added/launch/jgroups.sh
@@ -21,22 +21,59 @@ function configureEnv() {
 function configure_jgroups_encryption() {
   jgroups_encrypt=""
 
-  if [ -n "${JGROUPS_ENCRYPT_SECRET}" ]; then
-    if [ -n "${JGROUPS_ENCRYPT_NAME}" -a -n "${JGROUPS_ENCRYPT_PASSWORD}" ] ; then
-      # For new JGroups we need to use SYM_ENCRYPT protocol
-      jgroups_encrypt="\
-        <protocol type=\"SYM_ENCRYPT\">\
-          <property name=\"provider\">SunJCE</property>\
-          <property name=\"sym_algorithm\">AES</property>\
-          <property name=\"encrypt_entire_message\">true</property>\
-          <property name=\"keystore_name\">${JGROUPS_ENCRYPT_KEYSTORE_DIR}/${JGROUPS_ENCRYPT_KEYSTORE}</property>\
-          <property name=\"store_password\">${JGROUPS_ENCRYPT_PASSWORD}</property>\
-          <property name=\"alias\">${JGROUPS_ENCRYPT_NAME}</property>\
-        </protocol>"
-    else
-      log_warning "Partial JGroups encryption configuration, the communication within the cluster WILL NOT be encrypted."
-    fi
-  fi
+  JGROUPS_ENCRYPT_PROTOCOL="${JGROUPS_ENCRYPT_PROTOCOL:=SYM_ENCRYPT}"
+
+  case "${JGROUPS_ENCRYPT_PROTOCOL}" in
+    "SYM_ENCRYPT")
+      log_info "Configuring JGroups cluster traffic encryption protocol to SYM_ENCRYPT."
+      local JGROUPS_UNENCRYPTED_MESSAGE="Detected <STATE> JGroups encryption configuration, the communication within the cluster WILL NOT be encrypted."
+      local KEYSTORE_ERROR_MESSAGE=""
+      [ -n "${JGROUPS_ENCRYPT_SECRET}"       -a \
+        -n "${JGROUPS_ENCRYPT_NAME}"         -a \
+        -n "${JGROUPS_ENCRYPT_PASSWORD}"     -a \
+        -n "${JGROUPS_ENCRYPT_KEYSTORE_DIR}" -a \
+        -n "${JGROUPS_ENCRYPT_KEYSTORE}" ]
+      local KEYSTORE_DEFINITION_VALID="$?"
+      if [ "${KEYSTORE_DEFINITION_VALID}" -eq "0" ]; then
+        # For new JGroups we need to use SYM_ENCRYPT protocol
+        jgroups_encrypt="<protocol type=\"SYM_ENCRYPT\">\n\
+                        <property name=\"provider\">SunJCE</property>\n\
+                        <property name=\"sym_algorithm\">AES</property>\n\
+                        <property name=\"encrypt_entire_message\">true</property>\n\
+                        <property name=\"keystore_name\">${JGROUPS_ENCRYPT_KEYSTORE_DIR}/${JGROUPS_ENCRYPT_KEYSTORE}</property>\n\
+                        <property name=\"store_password\">${JGROUPS_ENCRYPT_PASSWORD}</property>\n\
+                        <property name=\"alias\">${JGROUPS_ENCRYPT_NAME}</property>\n\
+                    </protocol>"
+      elif [ -n "${JGROUPS_ENCRYPT_SECRET}" ]; then
+        KEYSTORE_ERROR_MESSAGE="${JGROUPS_UNENCRYPTED_MESSAGE//<STATE>/partial}"
+      else
+        KEYSTORE_ERROR_MESSAGE="${JGROUPS_UNENCRYPTED_MESSAGE//<STATE>/missing}"
+      fi
+
+      if [ -n "${KEYSTORE_ERROR_MESSAGE}" ]; then
+        log_warning "${KEYSTORE_ERROR_MESSAGE}"
+      fi
+      ;;
+    "ASYM_ENCRYPT")
+      log_info "Configuring JGroups cluster traffic encryption protocol to ASYM_ENCRYPT."
+      if [ -n "${JGROUPS_ENCRYPT_SECRET}"       -o \
+           -n "${JGROUPS_ENCRYPT_NAME}"         -o \
+           -n "${JGROUPS_ENCRYPT_PASSWORD}"     -o \
+           -n "${JGROUPS_ENCRYPT_KEYSTORE_DIR}" -o \
+           -n "${JGROUPS_ENCRYPT_KEYSTORE}" ] ; then
+        log_warning "The specified JGroups SYM_ENCRYPT JCEKS keystore definition will be ignored when using ASYM_ENCRYPT."
+      fi
+      # Asymmetric encryption using public/private encryption to fetch the shared secret key
+      jgroups_encrypt="<protocol type=\"ASYM_ENCRYPT\">\n\
+                      <property name=\"encrypt_entire_message\">true</property>\n\
+                      <property name=\"sym_keylength\">128</property>\n\
+                      <property name=\"sym_algorithm\">AES/ECB/PKCS5Padding</property>\n\
+                      <property name=\"asym_keylength\">512</property>\n\
+                      <property name=\"asym_algorithm\">RSA</property>\n\
+                      <property name=\"change_key_on_leave\">true</property>\n\
+                  </protocol>"
+      ;;
+  esac
 
   sed -i "s|<!-- ##JGROUPS_ENCRYPT## -->|$jgroups_encrypt|g" "$CONFIG_FILE"
 }
