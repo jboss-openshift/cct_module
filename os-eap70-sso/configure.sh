958224fff05dedb8e3143a730ce2510d03e6361e
Jan Lieskovsky <jlieskov@redhat.com>
2018-06-05 13:12:12 +0200

    [CLOUD-2295] Update RH-SSO client and SAML adapters to the RH-SSO 7.2.2 ones
    Signed-off-by: Jan Lieskovsky <jlieskov@redhat.com>

--- os-eap70-sso/configure.sh
+++ os-eap70-sso/configure.sh
@@ -4,8 +4,8 @@ SCRIPT_DIR=$(dirname $0)
 ADDED_DIR=${SCRIPT_DIR}/added
 SOURCES_DIR="/tmp/artifacts"
 
-unzip -o $SOURCES_DIR/rh-sso-7.1.0-eap7-adapter.zip -d $JBOSS_HOME
-unzip -o $SOURCES_DIR/rh-sso-7.1.2-saml-eap7-adapter.zip -d $JBOSS_HOME
+unzip -o $SOURCES_DIR/rh-sso-7.2.2-eap7-adapter.zip -d $JBOSS_HOME
+unzip -o $SOURCES_DIR/rh-sso-7.2.2-saml-eap7-adapter.zip -d $JBOSS_HOME
 
 chown -R jboss:root $JBOSS_HOME
 chmod -R g+rwX $JBOSS_HOME
