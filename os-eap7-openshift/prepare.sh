35018c1213fd064124b0464ac8703fd506f14548
Ken Wills <ken@zaptillion.net>
2018-10-04 17:57:18 -0500

    avoid creating /deployments/deployments if /deployments already exists

--- os-eap7-openshift/prepare.sh
+++ os-eap7-openshift/prepare.sh
@@ -6,7 +6,14 @@ ADDED_DIR=${SCRIPT_DIR}/added
 DEPLOYMENTS_DIR=/deployments
 
 # https://issues.jboss.org/browse/CLOUD-128
-mv $JBOSS_HOME/standalone/deployments $DEPLOYMENTS_DIR
+if [ ! -d "${DEPLOYMENTS_DIR}" ]; then
+  mv $JBOSS_HOME/standalone/deployments $DEPLOYMENTS_DIR
+else
+  # -T to avoid cping the deployments directory into the existing one
+  cp -R -T $JBOSS_HOME/standalone/deployments/ ${DEPLOYMENTS_DIR}
+  rm -rf $JBOSS_HOME/standalone/deployments
+fi
+
 ln -s /deployments $JBOSS_HOME/standalone/deployments
 chown jboss:root $JBOSS_HOME/standalone/deployments
 
