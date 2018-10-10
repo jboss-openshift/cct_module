165e6f1a7103367b333514dd551272047c75deb3
Ondra Chaloupka <ochaloup@redhat.com>
2018-10-10 13:04:21 +0200

    [CLOUD-2542] changes for differentiate eap6 and eap7 implementation of txn marker

--- os-eap64-modules/configure.sh
+++ os-eap64-modules/configure.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR=$(dirname $0)
 ADDED_DIR=${SCRIPT_DIR}/added
 SOURCES_DIR=/tmp/artifacts
 VERSION="1.0.3.Final-redhat-1"
+VERSION_TXN_MARKER="1.1.1.Final-redhat-00001"
 
 # Add new "openshift" layer
 # includes module definitions for OpenShift PING and OAuth
@@ -14,3 +15,5 @@ cp -rp --remove-destination "$ADDED_DIR/modules" "$JBOSS_HOME/"
 
 # Copy custom valves
 cp -p "${SOURCES_DIR}/tomcat-7-valves-$VERSION.jar" "$JBOSS_HOME/modules/system/layers/openshift/org/jboss/openshift/main/tomcat-7-valves.jar"
+cp -p "${SOURCES_DIR}/txn-recovery-marker-jdbc-common-${VERSION_TXN_MARKER}.jar" "$JBOSS_HOME/modules/system/layers/openshift/io/narayana/openshift-recovery/main/txn-recovery-marker-jdbc-common.jar"
+cp -p "${SOURCES_DIR}/txn-recovery-marker-jdbc-hibernate4-${VERSION_TXN_MARKER}.jar" "$JBOSS_HOME/modules/system/layers/openshift/io/narayana/openshift-recovery/main/txn-recovery-marker-jdbc-hibernate4.jar"
--- os-partition-txnrecovery/install_as_root.sh
+++ os-partition-txnrecovery/install_as_root.sh
@@ -4,17 +4,9 @@ set -e
 SCRIPT_DIR=$(dirname $0)
 ADDED_DIR=${SCRIPT_DIR}/added
 SOURCES_DIR="/tmp/artifacts"
-MODULE_DEST=${JBOSS_HOME}/modules/system/layers/openshift/io/narayana/openshift-recovery/main
 
 
 test -d /opt/partition || mkdir /opt/partition
 
 cp "$ADDED_DIR"/*.sh "$ADDED_DIR"/*.py /opt/partition/
 chmod 755 /opt/partition/*
-
-
-mkdir -p ${MODULE_DEST}
-cp "${ADDED_DIR}"/modules/system/layers/openshift/io/narayana/openshift-recovery/main/module.xml ${MODULE_DEST}
-cp ${SOURCES_DIR}/txn-recovery-marker-jdbc-*.jar ${MODULE_DEST}/txn-recovery-marker-jdbc.jar
-chown -R jboss:jboss ${JBOSS_HOME}/modules/system/layers/openshift/io/
-chmod 755 -R ${JBOSS_HOME}/modules/system/layers/openshift/io/
