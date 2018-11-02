b9ea715ca65f14b0696c38fe035acc1a217ab43e
Ken Wills <ken@zaptillion.net>
2018-11-02 17:42:19 -0500

    Correct module chmod path

--- jboss/container/openjdk/jdk/8/configure.sh
+++ jboss/container/openjdk/jdk/8/configure.sh
@@ -7,7 +7,7 @@ ARTIFACTS_DIR=${SCRIPT_DIR}/artifacts
 
 chown -R jboss:root $SCRIPT_DIR
 chmod -R ug+rwX $SCRIPT_DIR
-chmod ug+x ${ARTIFACTS_DIR}/opt/jboss/openjdk/jdk/*
+chmod ug+x ${ARTIFACTS_DIR}/opt/jboss/container/openjdk/jdk/*
 
 pushd ${ARTIFACTS_DIR}
 cp -pr * /
