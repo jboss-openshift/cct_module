d00642a1b137d0890d56921421173884724da8d8
Jonathan Dowland <jdowland@redhat.com>
2018-11-07 17:10:21 +0000

    add JAVA_DISABLE_TRUST_OPENSHIFT_CA to override CA trust
    When JAVA_DISABLE_TRUST_OPENSHIFT_CA is defined the trust-openshift-supplied-CA
    code will not run.
    
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- jboss/container/java/certs/bash/artifacts/trust-ose-cert.sh
+++ jboss/container/java/certs/bash/artifacts/trust-ose-cert.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-set -u
 set -e
 
-ca=/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt
-if test -r "$ca" ; then
-    cp "$ca" /etc/pki/ca-trust/source/anchors/
-    /usr/bin/update-ca-trust
+if [ -z "$JAVA_DISABLE_TRUST_OPENSHIFT_CA" ]; then
+    ca=/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt
+    if test -r "$ca" ; then
+        cp "$ca" /etc/pki/ca-trust/source/anchors/
+        /usr/bin/update-ca-trust
+    fi
 fi
