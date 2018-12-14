48fa25a2d5c65fd5a71fd75b390260f00ee21e55
Ken Wills <ken@zaptillion.net>
2018-12-14 09:44:42 -0600

    CLOUD-2954 - Move EAP modules out of cct_module

--- jboss/container/eap/prometheus/agent/configure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-SCRIPT_DIR=$(dirname $0)
-SRC_DIR=${SCRIPT_DIR}/src
-
-# Add prometheus configuration
-cat ${SRC_DIR}/standalone.conf >> $JBOSS_HOME/bin/standalone.conf
--- jboss/container/eap/s2i/bash/backward_compatibility.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Set up Hawkular for java s2i builder image
-set -e
-
-ln -s /opt/jboss/container/eap/s2i/install-common.sh /usr/local/s2i/install-common.sh
-
-chown -h jboss:root /usr/local/s2i/install-common.sh
