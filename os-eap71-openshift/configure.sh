1ced90793d90dd162492614228a170ebe682e73e
Ken Wills <ken@zaptillion.net>
2018-12-14 09:40:09 -0600

    CLOUD-2954 - Move EAP modules out of cct_module

--- os-eap71-openshift/configure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-SCRIPT_DIR=$(dirname $0)
-ADDED_DIR=${SCRIPT_DIR}/added
-
-# Add custom configuration file
-cp -p ${ADDED_DIR}/standalone-openshift.xml $JBOSS_HOME/standalone/configuration/
