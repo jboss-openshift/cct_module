46f28adf674a363c159c238ab07f4a159853697c
Howard Gao <howard.gao@gmail.com>
2019-01-03 16:35:07 +0800

    CLOUD-2957 Move AMQ modules out of cct_module
    Signed-off-by: Howard Gao <howard.gao@gmail.com>

--- os-amq-jolokia/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Add Jolokia (http://www.jolokia.org/) to expose all MBeans
-set -e
-
-SOURCES_DIR="/tmp/artifacts"
-
-# for backward compatibility.  jolokia is now located in /opt/jolokia.
-ln -s /opt/jolokia/jolokia.jar $AMQ_HOME/jolokia.jar
