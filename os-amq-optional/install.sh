dab9a4ae344190278ac8937aa905fc5c2fc13c89
Howard Gao <howard.gao@gmail.com>
2019-02-19 17:07:21 +0800

    CLOUD-2957 Move AMQ modules out of cct_module
    The amq modules are now moved to amq6 repository:
    https://github.com/jboss-container-images/jboss-amq-6-openshift-image.git
    
    Signed-off-by: Howard Gao <howard.gao@gmail.com>

--- os-amq-optional/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-SOURCES_DIR="/tmp/artifacts"
-
-cp -p "$SOURCES_DIR"/openshift-activemq-plugin-1.2.1.Final-redhat-1.jar $AMQ_HOME/lib/optional/
-cp -p "$SOURCES_DIR"/jboss-dmr-1.2.2.Final-redhat-1.jar $AMQ_HOME/lib/optional/
--- os-amq-secure-mgmt-console/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Secure the management console
-# NOTE: the script package os-amq-launch contains code to configure management
-# console credentials and access at runtime.
-set -e
-
-sed -i -e 's/0.0.0.0/localhost/' $AMQ_HOME/conf/jetty.xml
