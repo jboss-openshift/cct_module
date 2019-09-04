3b781973d4750071839ba43d7cb9f2a23355b405
Severin Gehwolf <sgehwolf@redhat.com>
2019-09-04 14:44:58 +0200

    [OPENJDK-38] Add support for Maven 3.6
    Signed-off-by: Severin Gehwolf <sgehwolf@redhat.com>

---
+++ jboss/container/maven/36/36scl/configure.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Configure module
+set -e
+
+SCRIPT_DIR=$(dirname $0)
+ARTIFACTS_DIR=${SCRIPT_DIR}/artifacts
+
+chown -R jboss:root $ARTIFACTS_DIR
+chmod -R ug+rwX $ARTIFACTS_DIR
+chmod ug+x ${ARTIFACTS_DIR}/opt/jboss/container/maven/36/scl-enable-maven
+
+pushd ${ARTIFACTS_DIR}
+cp -pr * /
+popd
+
+# maven pulls in jdk8, so we need to remove them if another jdk is the default
+if ! readlink /etc/alternatives/java | grep -q "java-1\.8\.0"; then
+    for pkg in java-1.8.0-openjdk-devel \
+               java-1.8.0-openjdk-headless \
+               java-1.8.0-openjdk; do
+        if rpm -q "$pkg"; then
+            rpm -e --nodeps "$pkg"
+        fi
+    done
+fi
