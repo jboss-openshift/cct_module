6e34a5c6bba2ff38137519b2bf44b330c581c7a6
Jonathan Dowland <jdowland@redhat.com>
2019-10-22 11:39:19 +0100

    initial openjdk.jdk.openj8-8 module
    forked from openjdk.jdk.8, and adapted
    
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

---
+++ jboss/container/openjdk/jdk/openj9-8/configure.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Configure module
+set -e
+
+SCRIPT_DIR=$(dirname $0)
+ARTIFACTS_DIR=${SCRIPT_DIR}/artifacts
+
+chown -R jboss:root $SCRIPT_DIR
+chmod -R ug+rwX $SCRIPT_DIR
+chmod ug+x ${ARTIFACTS_DIR}/opt/jboss/container/openjdk/jdk/*
+
+pushd ${ARTIFACTS_DIR}
+cp -pr * /
+popd
+
+# XXX still needed?
+# echo securerandom.source=file:/dev/urandom >> /usr/lib/jvm/java/jre/lib/security/java.security
