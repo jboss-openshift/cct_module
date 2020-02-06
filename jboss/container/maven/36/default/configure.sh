4f6851002da6de84b1324e1d81f8e1ee9dfee3a5
Jonathan Dowland <jdowland@redhat.com>
2020-02-06 14:06:51 +0000

    revamp rm-openjdk module as singleton-jdk
    Infer the correct Java packages that should be in the container
    from the environment, specifically JAVA_VERSION and JAVA_VENDOR.
    
    Remove similar code from the various Maven modules.
    
    Images which use the Maven modules should depend on "singleton-jdk"
    explicitly and ensure that it is sequenced after the Maven modules
    or any other module that might result in the installation of extra
    Java packages (e.g. jolokia…)
    
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- jboss/container/maven/36/default/configure.sh
+++ jboss/container/maven/36/default/configure.sh
@@ -1,16 +1,3 @@
 #!/bin/sh
 set -e
 
-dnf module enable -y maven:3.6
-dnf install -y --setopt=tsflags=nodocs maven
-
-# maven pulls in jdk8, so we need to remove them if another jdk is the default
-if ! readlink /etc/alternatives/java | grep -q "java-1\.8\.0"; then
-    for pkg in java-1.8.0-openjdk-devel \
-               java-1.8.0-openjdk-headless \
-               java-1.8.0-openjdk; do
-        if rpm -q "$pkg"; then
-            rpm -e --nodeps "$pkg"
-        fi
-    done
-fi
