260c2a021dc3de9f83c3ae8d17b128403a62f0fc
Jonathan Dowland <jdowland@redhat.com>
2020-02-17 11:48:42 +0000

    Add pre-check that JAVA_VENDOR and JAVA_VERSION aren't empty
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- jboss/container/java/singleton-jdk/configure.sh
+++ jboss/container/java/singleton-jdk/configure.sh
@@ -2,6 +2,15 @@
 set -u
 set -e
 
+if [ -z "$JAVA_VERSION" ]; then
+  echo "JAVA_VERSION needs to be defined to use this module" >&2
+  exit 1
+fi
+if [ -z "$JAVA_VENDOR" ]; then
+  echo "JAVA_VENDOR needs to be defined to use this module" >&2
+  exit 1
+fi
+
 # Clean up any java-* packages that have been installed that do not match
 # our stated JAVA_VERSION-JAVA_VENDOR (e.g.: 11-openjdk; 1.8.0-openj9)
 rpm -qa java-\* | while read pkg; do
