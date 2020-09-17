d35151f7d4b8771cd606bfcabde8e3a548155806
Jonathan Dowland <jdowland@redhat.com>
2020-09-17 11:50:45 +0100

    pkg-update: Address review comments
     • use "command" instead of "test"
     • execute the first package manager we find in the loop, instead
       of setting a variable for later
     • execute "clean all" with the package manager too

--- jboss/container/util/pkg-update/execute.sh
+++ jboss/container/util/pkg-update/execute.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-for mgr in yum dnf microdnf; do
-    if test -x "/usr/bin/$mgr"; then
-        mgr="/usr/bin/$mgr"
-        break
+for candidate in yum dnf microdnf; do
+    if command -v "$candidate"; then
+        mgr="$(command -v "$candidate")"
+        "$mgr" update -y
+        "$mgr" -y clean all
+        exit
     fi
 done
-if ! test -x "$mgr"; then
-    echo "cannot find a package manager" >&2
-    exit 1
-fi
 
-"$mgr" update -y
+echo "cannot find a package manager" >&2
+exit 1
