c8d8da7dda001794e6abfc9b7d5f0086f4d67b0a
Jonathan Dowland <jdowland@redhat.com>
2021-06-10 14:57:15 +0100

    [OPENJDK-407] New module: remove a set of non-UBI RPMs
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

---
+++ jboss/container/util/ubi-clean/configure.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+microdnf remove grub2-common \
+    dejavu-sans-mono-fonts \
+    memstrack \
+    os-prober \
+    grub2-tools \
+    grubby \
+    rpm-plugin-systemd-inhibit \
+    grub2-tools-minimal \
