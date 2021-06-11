d299333da359bf8e7c751cab67560b117dc3dfdc
Jonathan Dowland <jdowland@redhat.com>
2021-06-11 15:29:01 +0100

    ubi-clean module: Add an explanatory comment
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- jboss/container/util/ubi-clean/configure.sh
+++ jboss/container/util/ubi-clean/configure.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# This list of packages was arrived at by running the following in a
+# UBI/OpenJDK container, in an environment *without any redhat entitlements* to
+# be sure the repodata is ONLY that available via normal UBI channels and not
+# the wider RHEL8 or anything else:
+#   rpm -qa --queryformat "%{NAME}\n" \
+#   | while read pkg; do microdnf repoquery $pkg 2>/dev/null | grep -q $pkg || echo $pkg; done
+
 microdnf remove grub2-common \
     dejavu-sans-mono-fonts \
     memstrack \
